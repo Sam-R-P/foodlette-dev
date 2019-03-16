fd1101fd5f4327f28f4e91bfdf9fdc00e8be7dd5
Sam-R-P <48493741+Sam-R-P@users.noreply.github.com>
2019-03-16 17:50:28 +0000

    Add files via upload

--- serverFunctions.js
+++ serverFunctions.js
@@ -9,42 +9,50 @@ app.get('/', (req, res, next) => {
   next();
 });
 
-app.use(express.static('public'));
+app.use(express.static(''));
 
 app.listen(8080, function (req, res) {
   console.log('Foodlette listening on port 8080');
 });
 
 //---------- SERVER FUNCTIONS ----------//
+SubmitCreateAccount(email_1, email_2, password_1, password_2) {
+  //verify()? they arent empty? not all browsers check for empty inputs.
+
+  if (crossCheck(email_1, email_2) === false) {
+    sendMessage("Error", "Create an Account", "Your emails do not match, please try again.");
+  } else if (crossCheck(password_1, password_2) === false) {
+    sendMessage("Error", "Create an Account", "Your passwords do not match, please try again.");
+  } else if ("Then check DB for matching email") {
+    sendMessage("Error", "Create an Account", "Your email is already in use.");
+  } else {
+    //Otherwise create Account in DB and send succes message
+  }
+}
+
+//---------- SERVER UTILITIES ----------//
+sendMessage(title, about, message) {
+}
+
+crossCheck() {
+}
+
+
+
+
+//Needs to go client side
 createAccount() {
 }
 
 login() {
 }
 
-createTwoWheel() {
+createWheelTwo() {
 }
 
-createTenWheel() {
+createWheelTen() {
 }
 
-favouriteWheel() {
+WheelFav() {
 }
-
-validate() {
-}
-
-passwordValidate() {
-}
-
-crossCheck() {
-}
-
-SubmitCreateAccount() {
-  validate()
-  crossCheck()
-  crossCheck()
-}
-
-sendError() {
-}
+//
