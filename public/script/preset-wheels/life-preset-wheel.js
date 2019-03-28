3332d5fa725f7ec909d00e6d01cdf0e7e7ce7dec
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:12:05 +0000

    Update life-preset-wheel.js

--- public/script/preset-wheels/life-preset-wheel.js
+++ public/script/preset-wheels/life-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Life Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const Lifestyles = ["Lifestyles 1", "Lifestyles 2", "Lifestyles 3", "Lifestyles 4", "Lifestyles 5", "Lifestyles 6", "Lifestyles 7", "Lifestyles 8", "Lifestyles 9", "Lifestyles 10"];
+let options = Lifestyles;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
