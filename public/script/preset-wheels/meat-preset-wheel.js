f54d13cf8b8add10b97075dbdb3f08552f513f82
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:13:02 +0000

    Update meat-preset-wheel.js

--- public/script/preset-wheels/meat-preset-wheel.js
+++ public/script/preset-wheels/meat-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Meat Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const MeatFeasts = ["meat 1", "meat 2", "meat 3", "meat 4", "meat 5", "meat 6", "meat 7", "meat 8", "meat 9", "meat 10"];
+let options = MeatFeasts;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
