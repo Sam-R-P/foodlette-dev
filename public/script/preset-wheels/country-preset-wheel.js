843cf1a8224db691cd664d3b103b6702eafad4a1
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:09:22 +0000

    Update country-preset-wheel.js

--- public/script/preset-wheels/country-preset-wheel.js
+++ public/script/preset-wheels/country-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Country Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const CountryCuisines = ["country 1", "country 2", "country 3", "country 4", "country 5", "country 6", "country 7", "country 8", "country 9", "country 10"];
+let options = CountryCuisines;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
