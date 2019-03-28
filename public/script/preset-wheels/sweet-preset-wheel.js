df27e0e2ef62117907ca96d66f4be9b4e7236461
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:16:11 +0000

    Update sweet-preset-wheel.js

--- public/script/preset-wheels/sweet-preset-wheel.js
+++ public/script/preset-wheels/sweet-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Sweet Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const SweetTreats = ["sweet 1", "sweet 2", "sweet 3", "sweet 4", "sweet 5", "sweet 6", "sweet 7", "sweet 8", "sweet 9", "sweet 10"];
+let options = SweetTreats;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
