f827b93d9a294d5e39731b0c6feeeb69007a0370
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:14:09 +0000

    Update mood-preset-wheel.js

--- public/script/preset-wheels/mood-preset-wheel.js
+++ public/script/preset-wheels/mood-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Mood Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const MoodFoods = ["mood 1", "mood 2", "mood 3", "mood 4", "mood 5", "mood 6", "mood 7", "mood 8", "mood 9", "mood 10"];
+let options = MoodFoods;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
