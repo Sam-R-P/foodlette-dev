640fd28382aec5ff5e2ed937cb445dd11ac2e994
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:14:52 +0000

    Update pleasure-preset-wheel.js

--- public/script/preset-wheels/pleasure-preset-wheel.js
+++ public/script/preset-wheels/pleasure-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Pleasure Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const GuiltyPleasures = ["guilty 1", "guilty 2", "guilty 3", "guilty 4", "guilty 5", "guilty 6", "guilty 7", "guilty 8", "guilty 9", "guilty 10"];
+let options = GuiltyPleasures;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
