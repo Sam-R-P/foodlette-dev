4e109f3e16cc2a33a267a5bbf2e394dd0d255721
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:15:37 +0000

    Update spicy-preset-wheel.js

--- public/script/preset-wheels/spicy-preset-wheel.js
+++ public/script/preset-wheels/spicy-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Spicy Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const SpicySensations = ["spicy 1", "spicy 2", "spicy 3", "spicy 4", "spicy 5", "spicy 6", "spicy 7", "spicy 8", "spicy 9", "spicy 10"];
+let options = SpicySensations;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
