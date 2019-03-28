1b8c155ec6be8eebfee1924733cca90959863a39
up793414 <48527456+up793414@users.noreply.github.com>
2019-03-28 10:52:01 +0000

    Pushed open and close Navigation Menu

--- public/script/navsDialogues.js
+++ public/script/navsDialogues.js
@@ -1,1 +1,15 @@
 //----- Nav & Dialogues Eventlisteners Javascript -----//
+
+//--------- addNavEventListeners() &&& function addDialogueEventListeners() ----//
+//---------- &&& function openProfile() &&& function closeProfile() --------------//
+//----------"TAKE THIS OUT WHEN YOU PASTE" - tega ------//
+
+
+
+function openNavMenu() {
+  document.getElementById("navBar").style.right = "0px";
+}
+
+function closeNavMenu() {
+  document.getElementById("navBar").style.right = "-350px";
+}
