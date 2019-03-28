47f5bd6c80313bcf913e642a3c19615256a3da7e
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:07:59 +0000

    Update comfort-preset-wheel.js

--- public/script/preset-wheels/comfort-preset-wheel.js
+++ public/script/preset-wheels/comfort-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Comfort Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const ComfortCreations = ["comfort 1", "comfort 2", "comfort 3", "comfort 4", "comfort 5", "comfort 6", "comfort 7", "comfort 8", "comfort 9", "comfort 10"];
+let options = ComfortCreations;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
