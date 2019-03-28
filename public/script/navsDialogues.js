f98ee91c22f4a71c1c8fb9baaf1e767b9f621952
dijleseven <36961495+dijleseven@users.noreply.github.com>
2019-03-28 17:39:43 +0000

    updated navigation dialogue

--- public/script/navsDialogues.js
+++ public/script/navsDialogues.js
@@ -1,7 +1,40 @@
 //----- Nav & Dialogues Eventlisteners Javascript -----//
 
+
 //--------- addNavEventListeners() &&& function addDialogueEventListeners() ----//
 //---------- &&& function openProfile() &&& function closeProfile() --------------//
+
+function load() {
+  document.getElementById('onloadBox').style.display="block";
+}
+
+function closeBox() {
+  closeDefault();
+  document.getElementById("onloadBox").style.display = "none";
+}
+
+function openCreateAccount() {
+  closeDefault();
+  document.getElementById("createAccountForm").style.display = "block";
+}
+
+function openLogin() {
+  closeDefault();
+  document.getElementById("loginForm").style.display = "block";
+}
+
+//-----//
+
+function closeDefault() {
+  document.getElementById("flavourText").style.display = "none";
+  document.getElementById("guest").style.display = "none";
+  document.getElementById("createAccount").style.display = "none";
+  document.getElementById("login").style.display = "none";
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("createAccountForm").style.display = "none";
+}
+
+
 function openProfile() {
   document.getElementById("profileBar").style.right = "98px";
 }
@@ -10,7 +43,6 @@ function closeProfile() {
   document.getElementById("profileBar").style.right = "-350px";
 }
 //----------"TAKE THIS OUT WHEN YOU PASTE" - tega ------//
-
 
 
 function openNavMenu() {
