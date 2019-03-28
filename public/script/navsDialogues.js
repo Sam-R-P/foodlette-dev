1fc0d4a719a82555c803c1d8ca0669789d957875
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:20:43 +0000

    Update navsDialogues.js

--- public/script/navsDialogues.js
+++ public/script/navsDialogues.js
@@ -2,6 +2,13 @@
 
 //--------- addNavEventListeners() &&& function addDialogueEventListeners() ----//
 //---------- &&& function openProfile() &&& function closeProfile() --------------//
+function openProfile() {
+  document.getElementById("profileBar").style.right = "98px";
+}
+
+function closeProfile() {
+  document.getElementById("profileBar").style.right = "-350px";
+}
 //----------"TAKE THIS OUT WHEN YOU PASTE" - tega ------//
 
 
@@ -12,12 +19,4 @@ function openNavMenu() {
 
 function closeNavMenu() {
   document.getElementById("navBar").style.right = "-350px";
-}
-
-function openProfile() {
-  document.getElementById("profileBar").style.right = "98px";
-}
-
-function closeProfile() {
-  document.getElementById("profileBar").style.right = "-350px";
 }
