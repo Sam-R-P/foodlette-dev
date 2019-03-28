bb4724ef0f70e22d82f4fa0b195e1c5862da18e2
Deaston2017 <34191583+Deaston2017@users.noreply.github.com>
2019-03-28 14:11:13 +0000

    Update deal-preset-wheel.js

--- public/script/preset-wheels/deal-preset-wheel.js
+++ public/script/preset-wheels/deal-preset-wheel.js
@@ -1,1 +1,17 @@
 //----- Deal Preset Wheel Javascript -----//
+window.onload = loadScripts;
+
+function loadScripts() {
+  addNavEventListeners();
+  addDialogueEventListeners();
+  addWheelEventListeners();
+  drawRouletteWheel();
+}
+
+//WHEELS//
+const DelightfulDeals = ["delight 1", "delight 2", "delight 3", "delight 4", "delight 5", "delight 6", "delight 7", "delight 8", "delight 9", "delight 10"];
+let options = DelightfulDeals;
+
+function addWheelEventListeners() {
+  document.getElementById("spin").addEventListener("click", spin);
+}
