12232a2b865d4a55064134d5e9d9a28d900b0953
Ogbli <oliver.overton123@gmail.com>
2019-04-01 13:16:54 +0100

    Changed all html files so that the order is correct and matches the wheel. Also added pizza into comfort food. Furthermore, changed the navigation bar so that it is in alphabetical order.

--- public/script/preset-wheels/comfort-preset-wheel.js
+++ public/script/preset-wheels/comfort-preset-wheel.js
@@ -9,7 +9,7 @@ function loadScripts() {
 }
 
 //WHEELS//
-const ComfortCreations = ["comfort 1", "comfort 2", "comfort 3", "comfort 4", "comfort 5", "comfort 6", "comfort 7", "comfort 8", "comfort 9", "comfort 10"];
+const ComfortCreations = ["Pizza", "comfort 2", "comfort 3", "comfort 4", "comfort 5", "comfort 6", "comfort 7", "comfort 8", "comfort 9", "comfort 10"];
 let options = ComfortCreations;
 
 function addWheelEventListeners() {
