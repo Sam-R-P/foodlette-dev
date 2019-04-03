3592685b1fa40e6682805ce408f23af340cfc2a4
Oliver Overton <32401952+UP861808@users.noreply.github.com>
2019-04-03 13:41:28 +0100

    Updated loadScripts
    Removed Unneeded function and added beginning of documentation

--- public/script/myWheels.js
+++ public/script/myWheels.js
@@ -1,12 +1,17 @@
 //----- myWheels.HTML Javascript -----//
 window.onload = loadScripts;
 
+/**
+ * On window load the function loadScripts() loads in the required functions for page interactivity to work
+ */
 function loadScripts() {
   addNavEventListeners();
-  addDialogueEventListeners();
   addWheelCreators();
 }
 
+/**
+ * Adds eventlisteners for buttons to open and close the dialogue box as well as its intial options
+ */
 function addWheelCreators() {
   document.getElementById("createWheelButton").addEventListener("click", openWheelCreator);
   document.getElementById("creatorBoxClose").addEventListener("click", closeWheelCreator);
@@ -14,6 +19,9 @@ function addWheelCreators() {
   document.getElementById("wheelTenOptions").addEventListener("click", openTenWheel);
 }
 
+/**
+ * Sets the CSS properties for HTML elements based in button interactions
+ */
 function openWheelCreator() {
   document.getElementById("creatorBox").style.display = "flex";
   document.getElementById("wheelFlavourText").style.display = "block";
@@ -21,23 +29,35 @@ function openWheelCreator() {
   document.getElementById("wheelTenOptions").style.display = "flex";
 }
 
+/**
+ * Sets the CSS properties for HTML elements based in button interactions
+ */
 function closeWheelCreator() {
   document.getElementById("creatorBox").style.display = "none";
   document.getElementById("wheelTwoCreation").style.display = "none";
   document.getElementById("wheelTenCreation").style.display = "none";
 }
 
+/**
+ * Sets the CSS properties for HTML elements based in button interactions
+ */
 function closeOptionButtons() {
   document.getElementById("wheelFlavourText").style.display = "none";
   document.getElementById("wheelTwoOptions").style.display = "none";
   document.getElementById("wheelTenOptions").style.display = "none";
 }
 
+/**
+ * Sets the CSS properties for HTML elements based in button interactions
+ */
 function openTwoWheel() {
   closeOptionButtons();
   document.getElementById("wheelTwoCreation").style.display = "block";
 }
 
+/**
+ * Sets the CSS properties for HTML elements based in button interactions
+ */
 function openTenWheel() {
   closeOptionButtons();
   document.getElementById("wheelTenCreation").style.display = "block";
