7ac2f8e720eb10035549170238ddba6ff7c343af
Oliver Overton <32401952+UP861808@users.noreply.github.com>
2019-04-03 14:19:56 +0100

    Removed unneeded function
    And updated foods list

--- public/script/preset-wheels/pizza-preset-wheel.js
+++ public/script/preset-wheels/pizza-preset-wheel.js
@@ -8,7 +8,7 @@ function loadScripts() {
 }
 
 //WHEELS//
-const PremirePizza = ["Pizza", "Spaghetti", "Ice Cream", "comfort 4", "comfort 5", "comfort 6", "comfort 7", "comfort 8", "comfort 9", "comfort 10"];
+const PremirePizza = ["Pineapple", "Meat", "Spicy", "Margherita", "Pepperoni", "Chicken", "Meatball", "Stuffed Crust", "Thin Crust", "Vegetarian"];
 
 /**
  * Sets the value from the array as the values to be used on the roulette wheel
