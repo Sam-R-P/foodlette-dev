f1b248cec00a78dc7f4c3762ede65b8f6ce9d9de
Oliver Overton <32401952+UP861808@users.noreply.github.com>
2019-04-03 14:06:02 +0100

    Removed unneeded functions
    Also filled in meals on wheel

--- public/script/preset-wheels/deal-preset-wheel.js
+++ public/script/preset-wheels/deal-preset-wheel.js
@@ -3,13 +3,12 @@ window.onload = loadScripts;
 
 function loadScripts() {
   addNavEventListeners();
-  addDialogueEventListeners();
   addWheelEventListeners();
   drawRouletteWheel();
 }
 
 //WHEELS//
-const DelightfulDeals = ["delight 1", "delight 2", "delight 3", "delight 4", "delight 5", "delight 6", "delight 7", "delight 8", "delight 9", "delight 10"];
+const DelightfulDeals = ["Dominos", "McDonalds", "KFC", "Subway", "Burger King", "Meal Deal", "Giraffe", "Spoons", "Meerkat Meals", "Groupon"];
 let options = DelightfulDeals;
 
 function addWheelEventListeners() {
