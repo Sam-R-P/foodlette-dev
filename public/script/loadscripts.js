090ef7c6cfda1ae9a4da1f69885dc7dfc6ea8912
Oliver Overton <32401952+UP861808@users.noreply.github.com>
2019-04-03 13:35:11 +0100

    Added loadscripts.js
    Minimal script, used for loading signup and login pages.

---
+++ public/script/loadscripts.js
@@ -0,0 +1,8 @@
+window.onload = loadScripts;
+
+/**
+ * On window load the function loadScripts() loads in the required functions for page interactivity to work
+ */
+function loadScripts() {
+    addNavEventListeners();
+}
