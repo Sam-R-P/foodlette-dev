a34617bcf8aff73a0129eb8bf29b4e14f198eae3
Oliver Overton <32401952+UP861808@users.noreply.github.com>
2019-04-03 14:22:05 +0100

    Removed unneeded function
    Also updated foods

--- public/script/preset-wheels/pleasure-preset-wheel.js
+++ public/script/preset-wheels/pleasure-preset-wheel.js
@@ -3,13 +3,12 @@ window.onload = loadScripts;
 
 function loadScripts() {
   addNavEventListeners();
-  addDialogueEventListeners();
   addWheelEventListeners();
   drawRouletteWheel();
 }
 
 //WHEELS//
-const GuiltyPleasures = ["guilty 1", "guilty 2", "guilty 3", "guilty 4", "guilty 5", "guilty 6", "guilty 7", "guilty 8", "guilty 9", "guilty 10"];
+const GuiltyPleasures = ["Fudge", "Cookie Doe", "Cheese", "Cheese Cake", "Chocolate", "Sticky Toffee", "Bakery", "Doe Balls", "Pancakes", "Waffles"];
 let options = GuiltyPleasures;
 
 function addWheelEventListeners() {
